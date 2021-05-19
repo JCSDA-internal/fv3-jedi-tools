1fa1c44a8ff01ae07bb7bce2bc76731b6ad5afb8
Clementine Gas <43183478+cmgas@users.noreply.github.com>
2021-05-19 13:56:37 -0600

    Plot convergence for block minimizer (#11)
    * Adding files
    
    * Tool is working!
    
    * Add example yaml for block plots

--- src/fv3jeditools/__init__.py
+++ src/fv3jeditools/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
+from .diag_da_block_convergence import *
 from .diag_da_convergence import *
 from .diag_field_plot import *
 from .diag_hofx_innovations import *
