ec12b66b16db95be4b283eb0fb43ca5c3ba1a457
Benjamin Menetrier <benjamin.menetrier@irit.fr>
2022-04-14 11:27:56 +0200

    Fix scripts

--- src/raster/raster.py
+++ src/raster/raster.py
@@ -70,6 +70,10 @@ parser.add_argument("--ferret", dest="ferret", action="store_true", help="Ferret
 # Output file path
 parser.add_argument("--output", "-o", help="Output file path")
 
+# Lon/lat of view
+lonview = -45.0
+latview = 45.0
+
 # Parse arguments
 args = parser.parse_args()
 
@@ -78,9 +82,15 @@ args = parser.parse_args()
 # Set default string values
 if args.colormap is None:
     if args.centered:
-        args.colormap = "coolwarm"
+        if args.ferret:
+            args.colormap = "cmocean_balance"
+        else:
+            args.colormap = "seismic"
     else:
-        args.colormap = "jet"
+        if args.ferret:
+            args.colormap = "default"
+        else:
+            args.colormap = "viridis"
     
 # Print arguments
 print("Parameters:")
@@ -96,28 +106,49 @@ if (not (args.geos or args.gfs)):
 
 # -----------------------------------------------------------------------------
 
-# Preprocess fields with NCO operators
-
+# Import modules
+from netCDF4 import Dataset
+import numpy as np
+   
 if args.geos:
     # Check file extension
     if not args.filepath.endswith(".nc4"):
         print("   Error: filepath extension should be .nc4")
         sys.exit(1)
-    
-    # Extract variable
-    info = subprocess.getstatusoutput('ncks --help')
-    if info[0] == 0:
-        subprocess.run(["ncks", "-O", "-v", args.variable, args.filepath, "geos.nc"])
-    else:
-        print(info[1])
 
-    # Rename variable
-    info = subprocess.getstatusoutput('ncrename --help')
-    if info[0] == 0:
-        subprocess.run(["ncrename", "-v", args.variable + ",var", "geos.nc"])
-    else:
-        print(info[1])
+    # Open data file
+    fdata = Dataset(args.filepath, "r", format="NETCDF4")
 
+    # Read field
+    fld = fdata[args.variable][0,args.level-1,:,:,:]
+    units = fdata[args.variable].units
+    long_name = fdata[args.variable].long_name
+
+    if not args.basefilepath is None:
+        # Check base file extension
+        if not args.basefilepath.endswith(".nc4"):
+            print("   Error: basefilepath extension should be .nc4")
+            sys.exit(1)
+
+        # Open data file
+        fdata = Dataset(args.basefilepath, "r", format="NETCDF4")
+
+        # Variable name
+        if args.basevariable is None:
+            variable = args.variable
+        else:
+            variable = args.basevariable
+
+        # Read field
+        basefld = fdata[variable][0,args.level-1,:,:,:]
+
+        # Compute increment
+        fld = fld - basefld
+
+    # Get shape
+    shp = np.shape(fld)
+    ny = shp[1]
+    nx = shp[2]
 elif args.gfs:
     # Check file extension
     if not args.filepath.endswith(".nc"):
@@ -125,153 +156,128 @@ elif args.gfs:
         sys.exit(1)
 
     for itile in range(0, 6):
-        # Extract variable
+        # Open data file
         filename = args.filepath.replace(".nc", ".tile" + str(itile+1) + ".nc")
-        info = subprocess.getstatusoutput('ncks --help')
-        if info[0] == 0:
-            subprocess.run(["ncks", "-O", "-v", args.variable, filename, "gfs_tile" + str(itile) + ".nc"])
-        else:
-            print(info[1])
+        fdata = Dataset(filename, "r", format="NETCDF4")
 
-        # Rename variable
-        info = subprocess.getstatusoutput('ncrename --help')
-        if info[0] == 0:
-            subprocess.run(["ncrename", "-v", args.variable + ",var", "gfs_tile" + str(itile) + ".nc"])
-        else:
-            print(info[1])
+        # Read field
+        fld_tmp = fdata[args.variable][0,args.level-1,:,:]
+        units = fdata[args.variable].units
+        long_name = fdata[args.variable].long_name
 
-# TODO: other case (basefilename + basevariable) with ncdiff + temporary files cleaning at the end
+        if itile == 0:
+            # Get shape
+            shp = np.shape(fld_tmp)
+            ny = shp[0]
+            nx = shp[1]
 
+            # Initialize field
+            fld = np.zeros((6, ny, nx))
 
+        # Copy field
+        fld[itile,:,:] = fld_tmp
 
-# -----------------------------------------------------------------------------
+    if not args.basefilepath is None:
+        # Check base file extension
+        if not args.basefilepath.endswith(".nc"):
+            print("   Error: basefilepath extension should be .nc")
+            sys.exit(1)
+
+        for itile in range(0, 6):
+            # Open data file
+            filename = args.basefilepath.replace(".nc", ".tile" + str(itile+1) + ".nc")
+            fdata = Dataset(filename, "r", format="NETCDF4")
+
+            # Variable name
+            if args.basevariable is None:
+                variable = args.variable
+            else:
+                variable = args.basevariable
+
+            # Read field
+            fld_tmp = fdata[variable][0,args.level-1,:,:]
+
+            # Copy field
+            fld[itile,:,:] = fld[itile,:,:] - fld_tmp
+ 
+# Open grid file
+fgrid = Dataset(gridfiledir + "/fv3grid_c" + str(nx).zfill(4) + ".nc4", "r", format="NETCDF4")
+
+# Read grid vertices lons/lats
+vlons = np.degrees(fgrid["vlons"][:,:,:])
+vlats = np.degrees(fgrid["vlats"][:,:,:])
+
+# Figure title
+title = args.variable + " at level " + str(args.level) + " - C" + str(nx)
 
 if args.ferret:
+    # Write field in NetCDF file
+    if args.geos:
+        # TODO
+        print('not implemented yet')
+        exit()
+    elif args.gfs:
+        for itile in range(0, 6):
+            # Ferret file name
+            ferret_file_name = "ferret_tile"
+
+            # Open data file
+            filename = ferret_file_name + str(itile+1) + ".nc"
+            ncferret = Dataset(filename,mode="w",format="NETCDF4_CLASSIC")
+
+            # Create dimensions
+            nvx_dim = ncferret.createDimension('nvx', nx+1)
+            nvy_dim = ncferret.createDimension('nvy', ny+1)
+            ncx_dim = ncferret.createDimension('ncx', nx)
+            ncy_dim = ncferret.createDimension('ncy', ny)
+
+            # Create variables
+            lat = ncferret.createVariable('lat', np.float64, ('nvy','nvx',))
+            lat.units = 'degrees_north'
+            lat.long_name = 'latitude'
+            lon = ncferret.createVariable('lon', np.float64, ('nvy','nvx',))
+            lon.units = 'degrees_east'
+            lon.long_name = 'longitude'
+            var = ncferret.createVariable('var', np.float64, ('ncy','ncx',))
+            var.units = units
+            var.long_name = long_name
+
+            # Write variables
+            lat[:,:] = vlats[itile,:,:]
+            lon[:,:] = vlons[itile,:,:]
+            var[:,:] = fld[itile,:,:]
+
+            # Close file
+            ncferret.close()
+
     # Run ferret script
     info = subprocess.getstatusoutput('pyferret --help')
     if info[0] == 0:
-        subprocess.run(["pyferret", "-nodisplay", "-script", "raster.jnl"])
+        subprocess.run(["pyferret", "-nodisplay", "-script", "raster.jnl", ferret_file_name, title, str(lonview), str(latview), args.colormap, str(args.centered), args.output],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT)
     else:
         print(info[1])
+
+    # Remove temporary files
+    if args.geos:
+        # TODO
+        print('not implemented yet')
+        exit()
+    elif args.gfs:
+        for itile in range(0, 6):
+            os.remove(ferret_file_name + str(itile+1) + ".nc")
 else:
     # Import modules
-    from netCDF4 import Dataset
     import matplotlib.pyplot as plt
     import matplotlib.patches as mpatches
     import matplotlib.cm as cm
-    import numpy as np
-    import copy
     import cartopy.crs as ccrs
+    import copy
     
     # Projection
-    projection = ccrs.Robinson()
-    
-    # Lon/lat test
-    lon_test = False
-    lat_test = False
-    
-    if lon_test and lat_test:
-        print("ERROR: lon_test and lat_test are mutually exclusive")
-        sys.exit(1)
-    
-    if (lon_test or lat_test) and args.gfs:
-        print("ERROR: lon_test and lat_test are only available with GFS")
-        sys.exit(1)
-    
-    if args.geos:
-        # Check file extension
-        if not args.filepath.endswith(".nc4"):
-            print("   Error: filepath extension should be .nc4")
-            sys.exit(1)
-    
-        # Open data file
-        fdata = Dataset(args.filepath, "r", format="NETCDF4")
-    
-        if lon_test:
-            # Read lon
-            fld = fdata["lons"][:,:,:]
-        elif lat_test:
-            # Read lat
-            fld = fdata["lats"][:,:,:]
-        else:
-            # Read field
-            fld = fdata[args.variable][0,args.level-1,:,:,:]
-    
-        if not args.basefilepath is None:
-            # Check base file extension
-            if not args.basefilepath.endswith(".nc4"):
-                print("   Error: basefilepath extension should be .nc4")
-                sys.exit(1)
-    
-            # Open data file
-            fdata = Dataset(args.basefilepath, "r", format="NETCDF4")
-    
-            # Read field
-            if args.basevariable is None:
-                basefld = fdata[args.variable][0,args.level-1,:,:,:]
-            else:
-                basefld = fdata[args.basevariable][0,args.level-1,:,:,:]
-
-            # Compute increment
-            fld = fld - basefld
-    
-        # Get shape
-        shp = np.shape(fld)
-        ny = shp[1]
-        nx = shp[2]
-    elif args.gfs:
-        # Check file extension
-        if not args.filepath.endswith(".nc"):
-            print("   Error: filepath extension should be .nc")
-            sys.exit(1)
-    
-        for itile in range(0, 6):
-            # Open data file
-            filename = args.filepath.replace(".nc", ".tile" + str(itile+1) + ".nc")
-            fdata = Dataset(filename, "r", format="NETCDF4")
-    
-            # Read field
-            fld_tmp = fdata[args.variable][0,args.level-1,:,:]
-    
-            if itile == 0:
-                # Get shape
-                shp = np.shape(fld_tmp)
-                ny = shp[0]
-                nx = shp[1]
-    
-                # Initialize field
-                fld = np.zeros((6, ny, nx))
-    
-            # Copy field
-            fld[itile,:,:] = fld_tmp
-    
-        if not args.basefilepath is None:
-            # Check base file extension
-            if not args.basefilepath.endswith(".nc"):
-                print("   Error: basefilepath extension should be .nc")
-                sys.exit(1)
-    
-            for itile in range(0, 6):
-                # Open data file
-                filename = args.basefilepath.replace(".nc", ".tile" + str(itile+1) + ".nc")
-                fdata = Dataset(filename, "r", format="NETCDF4")
-    
-                # Read field
-                if args.basevariable is None:
-                    fld_tmp = fdata[args.variable][0,args.level-1,:,:]
-                else:
-                    fld_tmp = fdata[args.basevariable][0,args.level-1,:,:]
-
-                # Copy field
-                fld[itile,:,:] = fld[itile,:,:] - fld_tmp
- 
-    # Open grid file
-    fgrid = Dataset(gridfiledir + "/fv3grid_c" + str(nx).zfill(4) + ".nc4", "r", format="NETCDF4")
-    
-    # Read grid vertices lons/lats
-    vlons = np.degrees(fgrid["vlons"][:,:,:])
-    vlats = np.degrees(fgrid["vlats"][:,:,:])
-    
+    projection = ccrs.Orthographic(lonview, latview)
+   
     # Compute min/max
     if args.centered:
         vmax = np.max(np.abs(fld))
@@ -281,38 +287,24 @@ else:
         vmax = np.max(fld)
     norm = plt.Normalize(vmin=vmin, vmax=vmax)
     
-    # Compute averaging norm
-    normavg = 1.0/args.average**2
-    
     # Initialize figure
     fig,ax = plt.subplots(figsize=(8,8),subplot_kw=dict(projection=projection))
     ax.set_global()
     ax.coastlines()
-    ax.gridlines()
     
     # Colormap
     cmap = copy.copy(cm.get_cmap(args.colormap))
     cmap.set_bad('gray', 1)
     
     # Figure title
-    if args.average > 1:
-        plt.title(args.variable + " at level " + str(args.level) + " - C" + str(nx) + " - Avg. " + str(args.average))
-    else:
-        plt.title(args.variable + " at level " + str(args.level) + " - C" + str(nx))
+    plt.title(title)
     
     # Loop over tiles
     for itile in range(0, 6):
         # Loop over polygons
-        for iy in range(0, ny, args.average):
-            for ix in range(0, nx, args.average):
-                # Average value
-                value = 0.0
-                for iya in range(0, args.average):
-                    for ixa in range(0, args.average):
-                        value += fld[itile,iy+ixa,ix+ixa]
-                value = value*normavg
- 
-                if abs(value) >= args.threshold:
+        for iy in range(0, ny):
+            for ix in range(0, nx):
+                if abs(fld[itile,iy,ix]) >= args.threshold:
                     # Polygon coordinates      
                     xy = [[vlons[itile,iy+0,ix+0], vlats[itile,iy+0,ix+0]],
                           [vlons[itile,iy+0,ix+args.average], vlats[itile,iy+0,ix+args.average]],
@@ -320,15 +312,15 @@ else:
                           [vlons[itile,iy+args.average,ix+0], vlats[itile,iy+args.average,ix+0]]]
     
                     # Add polygon
-                    ax.add_patch(mpatches.Polygon(xy=xy, closed=True, facecolor=cmap(norm(value)),transform=ccrs.Geodetic()))
+                    ax.add_patch(mpatches.Polygon(xy=xy, closed=True, facecolor=cmap(norm(fld[itile,iy,ix])),transform=ccrs.Geodetic()))
     
     # Set colorbar
     sm = cm.ScalarMappable(cmap=args.colormap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
     sm.set_array([])
-    plt.colorbar(sm, orientation="horizontal", pad=0.06)
+    plt.colorbar(sm, orientation="vertical",shrink=0.8)
     
     # Save and close figure
-    plt.savefig(args.output + ".jpg", format="jpg", dpi=300)
+    plt.savefig(args.output + ".png", format="png", dpi=300)
     plt.close()
 
 # -----------------------------------------------------------------------------
@@ -336,7 +328,15 @@ else:
 # Trim figure with mogrify if available
 info = subprocess.getstatusoutput('mogrify -help')
 if info[0] == 0:
-    subprocess.run(["mogrify", "-trim", args.output + ".jpg"])
+    subprocess.run(["mogrify", "-trim", args.output + ".png"])
 
 # Print plot path
-print(" -> plot produced: " + args.output + ".jpg")
+print(" -> plot produced: " + args.output + ".png")
+
+# -----------------------------------------------------------------------------
+
+# Final time
+final_time = time.perf_counter()
+
+# Print timing
+print(f"raster.py executed in {final_time - initial_time:0.4f} seconds")
