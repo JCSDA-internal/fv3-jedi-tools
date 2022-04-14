98eacb768a26637e6aeca6b0a6ef954b9e3b12a5
Benjamin Menetrier <benjamin.menetrier@irit.fr>
2022-04-14 18:11:05 +0200

    Specific option for Orion

--- src/raster/raster.py
+++ src/raster/raster.py
@@ -14,7 +14,8 @@ import argparse
 import sys
 import time
 import subprocess
-
+import numpy as np
+from netCDF4 import Dataset
 
 # -----------------------------------------------------------------------------
 
@@ -27,6 +28,9 @@ initial_time = time.perf_counter()
 
 # FV3_GRID_DIR: directory with FV3 grid files for each resolution ("fv3grid_cNNNN.nc")
 gridfiledir = os.environ.get("FV3_GRID_DIR", os.path.dirname(os.path.realpath(__file__)) + "/fv3grid")
+
+# Host name
+hostname = os.environ.get("HOSTNAME", "")
 
 # -----------------------------------------------------------------------------
 
@@ -49,8 +53,8 @@ parser.add_argument("--basefilepath", "-bf", help="Base file path to compute a d
 # Base variable to compute a difference (optional)
 parser.add_argument("--basevariable", "-bv", help="Base variable")
 
-# Level
-parser.add_argument("--level", "-l", type=int, help="Level")
+# Levels
+parser.add_argument("--levels", "-l", type=str, help="Levels (values separated with commas)")
 
 # Color map (optional, default=jet or coolwarm)
 parser.add_argument("--colormap", "-cm", type=str, nargs="?", help="Color map (optional, default=jet or coolwarm)")
@@ -85,7 +89,7 @@ if args.colormap is None:
             args.colormap = "default"
         else:
             args.colormap = "viridis"
-    
+
 # Print arguments
 print("Parameters:")
 print(" - gridfiledir: " + gridfiledir)
@@ -94,16 +98,27 @@ for arg in vars(args):
         print(" - " + arg + ": " + str(getattr(args, arg)))
 
 # Check arguments
-if (not (args.geos or args.gfs)):
-    print("ERROR: --geos or --gfs required")
+if not (args.geos or args.gfs):
+    print("ERROR: --geos or --gfs is required")
     sys.exit(1)
+if args.filepath is None:
+    print("ERROR: filepath is required")
+    sys.exit(1)
+if args.variable is None:
+    print("ERROR: variable is required")
+    sys.exit(1)
+if args.levels is None:
+    print("ERROR: levels is required")
+    sys.exit(1)
+if args.output is None:
+    print("ERROR: output is required")
+    sys.exit(1)
+
+# Convert levels list to array of integers
+levels = np.array(list(map(int, args.levels.split(","))))
 
 # -----------------------------------------------------------------------------
 
-# Import modules
-from netCDF4 import Dataset
-import numpy as np
-   
 if args.geos:
     # Check file extension
     if not args.filepath.endswith(".nc4"):
@@ -114,7 +129,7 @@ if args.geos:
     fdata = Dataset(args.filepath, "r", format="NETCDF4")
 
     # Read field
-    fld = fdata[args.variable][0,args.level-1,:,:,:]
+    fld = fdata[args.variable][0,levels-1,:,:,:]
     units = fdata[args.variable].units
     long_name = fdata[args.variable].long_name
 
@@ -134,15 +149,16 @@ if args.geos:
             variable = args.basevariable
 
         # Read field
-        basefld = fdata[variable][0,args.level-1,:,:,:]
+        basefld = fdata[variable][0,levels-1,:,:,:]
 
         # Compute increment
         fld = fld - basefld
 
     # Get shape
     shp = np.shape(fld)
-    ny = shp[1]
-    nx = shp[2]
+    nz = shp[1]
+    ny = shp[2]
+    nx = shp[3]
 elif args.gfs:
     # Check file extension
     if not args.filepath.endswith(".nc"):
@@ -155,21 +171,23 @@ elif args.gfs:
         fdata = Dataset(filename, "r", format="NETCDF4")
 
         # Read field
-        fld_tmp = fdata[args.variable][0,args.level-1,:,:]
+        fld_tmp = fdata[args.variable][0,levels-1,:,:]
+
         units = fdata[args.variable].units
         long_name = fdata[args.variable].long_name
 
         if itile == 0:
             # Get shape
             shp = np.shape(fld_tmp)
-            ny = shp[0]
-            nx = shp[1]
+            nz = shp[0]
+            ny = shp[1]
+            nx = shp[2]
 
             # Initialize field
-            fld = np.zeros((6, ny, nx))
+            fld = np.zeros((6, nz, ny, nx))
 
         # Copy field
-        fld[itile,:,:] = fld_tmp
+        fld[itile,:,:,:] = fld_tmp
 
     if not args.basefilepath is None:
         # Check base file extension
@@ -189,20 +207,26 @@ elif args.gfs:
                 variable = args.basevariable
 
             # Read field
-            fld_tmp = fdata[variable][0,args.level-1,:,:]
+            fld_tmp = fdata[variable][0,levels-1,:,:]
 
             # Copy field
-            fld[itile,:,:] = fld[itile,:,:] - fld_tmp
- 
+            fld[itile,:,:,:] = fld[itile,:,:,:] - fld_tmp
+
+
+# Compute min/max
+if args.centered:
+    vmax = np.max(np.abs(fld))
+    vmin = -vmax
+else:
+    vmin = np.min(fld)
+    vmax = np.max(fld)
+
 # Open grid file
 fgrid = Dataset(gridfiledir + "/fv3grid_c" + str(nx).zfill(4) + ".nc4", "r", format="NETCDF4")
 
 # Read grid vertices lons/lats
 vlons = np.degrees(fgrid["vlons"][:,:,:])
 vlats = np.degrees(fgrid["vlats"][:,:,:])
-
-# Figure title
-title = args.variable + " at level " + str(args.level) + " - C" + str(nx)
 
 if args.ferret:
     # Write field in NetCDF file
@@ -224,6 +248,7 @@ if args.ferret:
             nvy_dim = ncferret.createDimension('nvy', ny+1)
             ncx_dim = ncferret.createDimension('ncx', nx)
             ncy_dim = ncferret.createDimension('ncy', ny)
+            nz_dim = ncferret.createDimension('nz', nz)
 
             # Create variables
             lat = ncferret.createVariable('lat', np.float64, ('nvy','nvx',))
@@ -232,27 +257,100 @@ if args.ferret:
             lon = ncferret.createVariable('lon', np.float64, ('nvy','nvx',))
             lon.units = 'degrees_east'
             lon.long_name = 'longitude'
-            var = ncferret.createVariable('var', np.float64, ('ncy','ncx',))
+            var = ncferret.createVariable('var', np.float64, ('nz','ncy','ncx',))
             var.units = units
             var.long_name = long_name
 
             # Write variables
             lat[:,:] = vlats[itile,:,:]
             lon[:,:] = vlons[itile,:,:]
-            var[:,:] = fld[itile,:,:]
+            var[:,:,:] = fld[itile,:,:,:]
 
             # Close file
             ncferret.close()
 
-    # Run ferret script
-    info = subprocess.getstatusoutput('ferret --help')
-    if info[0] == 0:
-        subprocess.run(["ferret", "-unmapped", "-gif", "-script", "raster.jnl", ferret_file_name, title, str(lonview), str(latview), args.colormap, str(args.centered), args.output],
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.STDOUT)
-    else:
-        print(info[1])
+for iz in range(0, nz):
+    # Figure title
+    title = long_name + " [" + units + "] at level " + str(levels[iz]) + " - C" + str(nx)
 
+    # Output
+    output = args.output + "_" + str(levels[iz])
+    
+    if args.ferret:
+        if "Orion" in hostname:
+            # Run ferret script (pyferret not available on Orion)
+            info = subprocess.getstatusoutput('ferret -help')
+            if info[0] == 0:
+                subprocess.run(["ferret", "-unmapped", "-gif", "-script", "raster_orion.jnl", ferret_file_name, str(iz+1), "\"" + title + "\"", str(lonview), str(latview), args.colormap, str(vmin), str(vmax), output],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.STDOUT)
+            else:
+                print(info[1])
+        else:
+            # Run pyferret script
+            info = subprocess.getstatusoutput('pyferret -help')
+            if info[0] == 0:
+                subprocess.run(["pyferret", "-png", "-script", "raster.jnl", ferret_file_name, str(iz+1), title, str(lonview), str(latview), args.colormap, str(vmin), str(vmax), output],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.STDOUT)
+            else:
+                print(info[1])
+    else:
+        # Import modules
+        import matplotlib.pyplot as plt
+        import matplotlib.patches as mpatches
+        import matplotlib.cm as cm
+        import cartopy.crs as ccrs
+        import copy
+    
+        # Projection
+        projection = ccrs.Orthographic(lonview, latview)
+        
+        # Initialize figure
+        fig,ax = plt.subplots(figsize=(8,8),subplot_kw=dict(projection=projection))
+        ax.set_global()
+        ax.coastlines()
+    
+        # Colormap
+        cmap = copy.copy(cm.get_cmap(args.colormap))
+        cmap.set_bad('gray', 1)
+    
+        # Figure title
+        plt.title(title)
+    
+        # Loop over tiles
+        for itile in range(0, 6):
+            # Loop over polygons
+            for iy in range(0, ny):
+                for ix in range(0, nx):
+                    # Polygon coordinates
+                    xy = [[vlons[itile,iy+0,ix+0], vlats[itile,iy+0,ix+0]],
+                          [vlons[itile,iy+0,ix+1], vlats[itile,iy+0,ix+1]],
+                          [vlons[itile,iy+1,ix+1], vlats[itile,iy+1,ix+1]],
+                          [vlons[itile,iy+1,ix+0], vlats[itile,iy+1,ix+0]]]
+    
+                    # Add polygon
+                    ax.add_patch(mpatches.Polygon(xy=xy, closed=True, facecolor=cmap(norm(fld[itile,iy,ix])),transform=ccrs.Geodetic()))
+    
+        # Set colorbar
+        sm = cm.ScalarMappable(cmap=args.colormap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
+        sm.set_array([])
+        plt.colorbar(sm, orientation="vertical",shrink=0.8)
+    
+        # Save and close figure
+        plt.savefig(output + ".png", format="png", dpi=300)
+        plt.close()
+    
+    # Trim figure with mogrify if available
+    info = subprocess.getstatusoutput('mogrify -help')
+    if info[0] == 0:
+        if args.ferret and "Orion" in hostname:
+            subprocess.run(["mogrify", "-trim", "-format", "png", output + ".gif"])
+            os.remove(output + ".gif")
+        else:
+            subprocess.run(["mogrify", "-trim", output + ".png"])
+
+if args.ferret:
     # Remove temporary files
     if args.geos:
         # TODO
@@ -260,72 +358,7 @@ if args.ferret:
         exit()
     elif args.gfs:
         for itile in range(0, 6):
-            os.remove(ferret_file_name + str(itile+1) + ".nc")
-else:
-    # Import modules
-    import matplotlib.pyplot as plt
-    import matplotlib.patches as mpatches
-    import matplotlib.cm as cm
-    import cartopy.crs as ccrs
-    import copy
-    
-    # Projection
-    projection = ccrs.Orthographic(lonview, latview)
-   
-    # Compute min/max
-    if args.centered:
-        vmax = np.max(np.abs(fld))
-        vmin = -vmax
-    else:
-        vmin = np.min(fld)
-        vmax = np.max(fld)
-    norm = plt.Normalize(vmin=vmin, vmax=vmax)
-    
-    # Initialize figure
-    fig,ax = plt.subplots(figsize=(8,8),subplot_kw=dict(projection=projection))
-    ax.set_global()
-    ax.coastlines()
-    
-    # Colormap
-    cmap = copy.copy(cm.get_cmap(args.colormap))
-    cmap.set_bad('gray', 1)
-    
-    # Figure title
-    plt.title(title)
-    
-    # Loop over tiles
-    for itile in range(0, 6):
-        # Loop over polygons
-        for iy in range(0, ny):
-            for ix in range(0, nx):
-                # Polygon coordinates      
-                xy = [[vlons[itile,iy+0,ix+0], vlats[itile,iy+0,ix+0]],
-                      [vlons[itile,iy+0,ix+1], vlats[itile,iy+0,ix+1]],
-                      [vlons[itile,iy+1,ix+1], vlats[itile,iy+1,ix+1]],
-                      [vlons[itile,iy+1,ix+0], vlats[itile,iy+1,ix+0]]]
-    
-                # Add polygon
-                ax.add_patch(mpatches.Polygon(xy=xy, closed=True, facecolor=cmap(norm(fld[itile,iy,ix])),transform=ccrs.Geodetic()))
-    
-    # Set colorbar
-    sm = cm.ScalarMappable(cmap=args.colormap, norm=plt.Normalize(vmin=vmin, vmax=vmax))
-    sm.set_array([])
-    plt.colorbar(sm, orientation="vertical",shrink=0.8)
-    
-    # Save and close figure
-    plt.savefig(args.output + ".png", format="png", dpi=300)
-    plt.close()
-
-# -----------------------------------------------------------------------------
-
-# Trim figure with mogrify if available
-info = subprocess.getstatusoutput('mogrify -help')
-if info[0] == 0:
-    if args.ferret:
-        subprocess.run(["mogrify", "-trim", "-format", "png", args.output + ".gif"])
-        os.remove(args.output + ".gif")
-    else:
-        subprocess.run(["mogrify", "-trim", args.output + ".png"])
+            os.remove(ferret_file_name + str(itile+1) + ".nc")    
 
 # -----------------------------------------------------------------------------
 
